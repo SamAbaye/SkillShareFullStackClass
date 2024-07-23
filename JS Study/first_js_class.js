15240ce54baa8d8b2720fb273c44107299430bad
[Smiblessed] <[samitube30@gmail.com]>
2024-07-23 21:40:10 +0300

    New Exercise

---
+++ JS Study/first_js_class.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+let object = {
+   name: 'Samson',
+   age: 34,
+   isStudent: true,
+   address: {
+    city: 'Addis Ababa',
+    sub_City: 'Bole'
+   }
+
+};
+
+console.log(object);
