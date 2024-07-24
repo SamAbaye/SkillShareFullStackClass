3b60deb28341f009825d685959acdac755eeae0e
[Smiblessed] <[samitube30@gmail.com]>
2024-07-24 18:07:11 +0300

    Update

--- JS Study/Task2.js
+++ JS Study/Task2.js
@@ -1,4 +1,4 @@
-let object = {
+export let object = {
     name: 'Samson',
     age: 34,
     isStudent: true,
@@ -6,9 +6,8 @@ let object = {
      city: 'Addis Ababa',
      sub_City: 'Bole'
     },
-    introduce: {
-        run: console.log(`My name is ${object.name}, I am ${object.age} Years old. it is ${object.isStudent} that am a student and i live in ${object.address.city} around ${object.address.sub_City}`)
+    introduce: function() {
+        console.log(`My name is ${object.name}, I am ${object.age} Years old. it is ${object.isStudent} that am a student and i live in ${object.address.city} around ${object.address.sub_City}`)
     }
  };
-console.log(object.age);
-export {object};
+console.log(object.introduce());
